977093fda4c4fc7f06771bf5749b25dbd769f768
lore <751029317@qq.com>
2022-03-02 20:10:17 +0800

    test auto deploy

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class About extends Component {
   render() {
     return (
-      <h1>this is tencent cloudbase</h1>
+      <h1>tencent cloudbase ready</h1>
     )
   }
 }
