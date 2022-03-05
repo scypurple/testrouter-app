34b0953ec385deca16fbd6f42f7b335fca866307
lore <751029317@qq.com>
2022-03-05 17:11:52 +0800

    refresh 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRoutes } from 'react-router-dom'
+import { useRoutes, useNavigationType, Navigate } from 'react-router-dom'
 import MyNavLink from './components/MyNavLink'
 import routes from './routes'
 import Header from './components/Header'
@@ -7,35 +7,43 @@ import Header from './components/Header'
 export default function App() {
 
     const element = useRoutes(routes);
-    return (
-        <div>
-            <div className="row">              
-             <Header />
-            </div>
+    const urlParams = new URL(window.location.href);
+    const pathname = urlParams?.pathname;
+   // console.log("pathname:", pathname);
+    return (useNavigationType() === 'POP') ? <Navigate to={pathname} /> :
+        (
 
-            <div className="row">
-                <div className="col-xs-2 col-xs-offset-2">
-                    <div className="list-group">
 
-                        {/* 原生html中使用a标签实现【页面】的【跳转】 */}
-                        {/* <a className="list-group-item" href="./about.html">About</a>
+
+
+            <div>
+                <div className="row">
+                    <Header />
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+
+                            {/* 原生html中使用a标签实现【页面】的【跳转】 */}
+                            {/* <a className="list-group-item" href="./about.html">About</a>
                     <a className="list-group-item" href="./home.html">Home</a> */}
 
-                        {/* react中使用Link标签实现【组件】的【切换】------编写路由导航 */}
-                        <MyNavLink to='/about'>About</MyNavLink>
-                        <MyNavLink to='/home'>Home</MyNavLink>
+                            {/* react中使用Link标签实现【组件】的【切换】------编写路由导航 */}
+                            <MyNavLink to='/about'>About</MyNavLink>
+                            <MyNavLink end to='/home'>Home</MyNavLink>
+                        </div>
                     </div>
-                </div>
-                <div className="col-xs-6">
-                    <div className="panel">
-                        <div className="panel-body">
-                            {/* 展示区要根据路径去决定展示哪个组件----注册路由 */}
-                            {element}
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                {/* 展示区要根据路径去决定展示哪个组件----注册路由 */}
+                                {element}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
 
