d76178fbb5365587ba2d6bfa44b20ae941f634cc
lore <751029317@qq.com>
2022-03-05 16:39:37 +0800

    useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
-import {useRoutes} from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 import MyNavLink from './components/MyNavLink'
 import routes from './routes'
+import Header from './components/Header'
 
 export default function App() {
 
     const element = useRoutes(routes);
-        return (
-            <div>
-                <div className="row">
-                    <div className="col-xs-offset-2 col-xs-8">
-                        <div className="page-header"><h2>React Router Demo</h2></div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-2">
-                        <div className="list-group">
+    return (
+        <div>
+            <div className="row">              
+             <Header />
+            </div>
 
-                            {/* 原生html中使用a标签实现【页面】的【跳转】 */}
-                            {/* <a className="list-group-item" href="./about.html">About</a>
+            <div className="row">
+                <div className="col-xs-2 col-xs-offset-2">
+                    <div className="list-group">
+
+                        {/* 原生html中使用a标签实现【页面】的【跳转】 */}
+                        {/* <a className="list-group-item" href="./about.html">About</a>
                     <a className="list-group-item" href="./home.html">Home</a> */}
 
-                            {/* react中使用Link标签实现【组件】的【切换】------编写路由导航 */}
-                            <MyNavLink to='/about'>About</MyNavLink>
-                            <MyNavLink to='/home'>Home</MyNavLink>
-                        </div>
+                        {/* react中使用Link标签实现【组件】的【切换】------编写路由导航 */}
+                        <MyNavLink to='/about'>About</MyNavLink>
+                        <MyNavLink to='/home'>Home</MyNavLink>
                     </div>
-                    <div className="col-xs-6">
-                        <div className="panel">
-                            <div className="panel-body">
-                                {/* 展示区要根据路径去决定展示哪个组件----注册路由 */}
-                            {element} 
-                            </div>
+                </div>
+                <div className="col-xs-6">
+                    <div className="panel">
+                        <div className="panel-body">
+                            {/* 展示区要根据路径去决定展示哪个组件----注册路由 */}
+                            {element}
                         </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
 
